b653e57d1fae9b18ddb8f281f8488e18fcb94e1f
patrick dorgu <pat.dorgu@gmail.com>
2020-04-02 17:30:33 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -1,25 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
-import "./App.css";
+import Header from "./components/Header";
+
 
 function App() {
-  document.title = "user Directory";
   return (
-    <Router>
-      <div>
-    <Navbar />
+  <div className="App">
     <Wrapper>
- <Route exact path="/" component={HomePage} />
+      <Header />
+      <Main />
     </Wrapper>
-    <Footer />
-     </div>
-    
-    </Router>
+  </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/App';
+import App from '../src/app';
 import * as serviceWorker from './components/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
